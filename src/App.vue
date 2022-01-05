f5e71f2739f053c10bf83151bc58056217d598cb
Abhijit Paul <paulabhi0022@gmail.com>
2022-01-05 17:35:23 +0530

    route added

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,35 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <i class="fab fa-500px"></i>-->
 
-      <form1 />
-      <form2 />
+     <!-- <form1 />
+      <form2 />-->
+      <ul>
+        <li>
+          <router-link to="/form1">Design 1</router-link>
+        </li>
+
+        <li>
+          <router-link to="/form2">Design 2</router-link>
+        </li>
+
+
+      </ul>
+      <router-view></router-view>
 
   </div>
 </template>
 
 <script>
 //import HelloWorld from "./components/HelloWorld.vue";
-import form1 from './components/form1.vue'
-import form2 from './components/form2.vue'
+//import form1 from './components/form1.vue'
+//import form2 from './components/form2.vue'
 
 export default {
   name: "App",
   components: {
     //HelloWorld,
-    form1,
-    form2,
+   // form1,
+    //form2,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,22 @@ import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import form1 from './components/form1'
+import form2 from './components/form2'
 
+Vue.use(VueRouter);
+const routes =[
+  {path:'/form1',component:form1},
+  {path:'/form2',component:form2}
+]
+
+const router = new VueRouter({
+  routes
+})
 Vue.config.productionTip = false
 
 new Vue({
+  router:router,
   render: h => h(App),
 }).$mount('#app')
